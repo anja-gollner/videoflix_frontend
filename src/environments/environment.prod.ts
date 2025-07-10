f1d6e671c9abbf2b2c92d9a7852eeeac8e8f2c99
Anja Gollner <anjakgollner@gmail.com>
2025-07-10 11:32:42 +0200

    prod variables

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://videoflix-backend-3zyr.onrender.com' // Passe an deine Render-URL an
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false, // Ändere zu true für Produktion
+  apiUrl: 'http://localhost:4200/api/' // Platzhalter für lokale Entwicklung
+};
